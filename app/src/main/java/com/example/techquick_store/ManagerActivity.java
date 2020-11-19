d838f26d1cd98d25642cb28d4c0649056035e857
User <ernar200201kz@gmail.com>
2020-11-19 22:24:11 +0600

    added search function for manager activity

--- app/src/main/java/com/example/techquick_store/ManagerActivity.java
+++ app/src/main/java/com/example/techquick_store/ManagerActivity.java
@@ -5,8 +5,10 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,17 +26,33 @@ public class ManagerActivity extends AppCompatActivity {
 
         userLoginHelper = new UserLoginHelper(this);
         sqLiteDatabase = userLoginHelper.getWritableDatabase();
-        Cursor cursor = sqLiteDatabase.query(userLoginHelper.TABLE_NAME,null,null,null,null,null,null,null);
 
+        setData(null);
+
+
+
+    }
+
+
+    public void setData(String query){
+        Cursor cursor;
+        if(query == null)
+            cursor = sqLiteDatabase.query(userLoginHelper.TABLE_NAME,null,null,null,null,null,null,null);
+
+        else{
+            cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + UserLoginHelper.TABLE_NAME +
+                    " WHERE " + UserLoginHelper.USER_COLUMN_NAME +  " LIKE " + "'%" +query+"%'" +
+                    " OR " + UserLoginHelper.USER_COLUMN_SURNAME + " LIKE " + "'%" +query+"%'",null);
+        }
 
 
         users_container = findViewById(R.id.container_for_users);
 
 
 
-        if (cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             do {
-                View view = getLayoutInflater().inflate(R.layout.one_user_manager_layout,null,false);
+                View view = getLayoutInflater().inflate(R.layout.one_user_manager_layout, null, false);
 
                 user_id = view.findViewById(R.id.user_id);
                 user_name = view.findViewById(R.id.user_name);
@@ -48,21 +66,30 @@ public class ManagerActivity extends AppCompatActivity {
                 user_status.setText(cursor.getString(cursor.getColumnIndex(UserLoginHelper.USER_COLUMN_STATUS)));
 
                 //Set blocked users background red
-                if(cursor.getString(cursor.getColumnIndex(UserLoginHelper.USER_COLUMN_STATUS)).equals("Blocked")){
+                if (cursor.getString(cursor.getColumnIndex(UserLoginHelper.USER_COLUMN_STATUS)).equals("Blocked")) {
                     user_status.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
                 }
 
                 users_container.addView(view);
 
-            }while (cursor.moveToNext());
+            } while (cursor.moveToNext());
         }
-
-
-
-
     }
 
 
+
+    public void searchClicked(View view){
+        users_container.removeAllViews();
+        String search_edit_text;
+        EditText editText = findViewById(R.id.search_edit_text);
+        search_edit_text = editText.getText().toString();
+        if(search_edit_text != null){
+            setData(search_edit_text);
+        }
+        else{
+            Toast.makeText(this,"Please write text for search",Toast.LENGTH_SHORT).show();
+        }
+    }
 
 
     public void changeStatus(View view){
